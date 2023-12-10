02aac9326e5d5d7e3fb7b7ccb1e9d0f9edcfb0ae
Arslan Qaisar <arslan.qaisar.dev@gmail.com>
2023-12-10 18:16:37 +0500

    first commit

---
+++ app/components/Footer/footer.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+
+<footer id="footer" className="section footer">
+        <div className="container">
+          <div className="footer__top">
+            <div className="footer-top__box">
+              <h3>MyDoc</h3>
+              <a href="#">Free Guidance</a>
+              <a href="#">24 hours services</a>
+              <a href="#">Reasonable Prices</a>
+              <a href="#">Friendly environment</a>
+            </div>
+            <div className="footer-top__box">
+              <h3>INFORMATION</h3>
+              <a href="#">About Us</a>
+              <a href="#">Privacy Policy</a>
+              <a href="#">Terms &amp; Conditions</a>
+              <a href="#">Contact Us</a>
+            </div>
+            <div className="footer-top__box">
+              <h3>CONTACT US</h3>
+              <div>
+                123 street Main Posh Area Faisalabad
+              </div>
+              <div>
+                Arslan.qaisar.dev@gmail.com
+              </div>
+              <div>
+                Arslan Qaisar
+              </div>
+              <div>
+                Dream City, Faislabad
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
+    </div>
+  )
+}
